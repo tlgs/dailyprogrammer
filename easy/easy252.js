136e3ba23f9f75b203cdd270c0dc698478f5aade
tlseabra <tiagolseabra@gmail.com>
2016-02-01 23:10:26 +0100

    change file names + easy #252
    Files names changes so they appear by order in the folder
    added easy #252

---
+++ easy/easy252.js
@@ -0,0 +1,26 @@
+/* 01/02/2016 */
+
+//Brute-force implementation
+
+function nightStash(s, c){
+  
+  for(var i = 0; i < s; i++){
+    if(c % s !== 1 || Math.floor( c/s ) == 0) return null;
+    c -= (Math.floor( c/s ) + 1);
+  }
+  return c;
+}
+
+var sailors = prompt("How many sailors are stranded in the isle?"),
+    test,
+    coconuts = 1;
+
+while(true){
+  test = nightStash(sailors, coconuts);
+  if( test !== null && (test % sailors === 0))
+    break;
+  else
+    coconuts ++;
+}
+
+console.log(sailors + " sailors need " + coconuts + " coconuts!" );
