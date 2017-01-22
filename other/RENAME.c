c4f3da90616e1a715aa620d9901b13a4cd46b2e7
Tiago Seabra <tiagolseabra@gmail.com>
2017-01-22 00:16:00 +0000

    file rename: "easyxxx" --> "exxx"
    addition of the "other" folder for assorted scripts.

---
+++ other/RENAME.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define MAXFILES 300
+#define EXTENSION ""
+
+int main(void){
+    for(int i = 0; i < MAXFILES; i++){
+        char old[256], new[256];
+        snprintf(old, sizeof(old), "%s%03d%s", "easy", i, EXTENSION);
+        if(access(old, F_OK) != -1){
+            snprintf(new, sizeof(new), "%s%03d%s", "e", i, EXTENSION);
+            rename(old, new);
+        }
+    }
+}
