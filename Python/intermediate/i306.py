55d16b7194ce008b4d148bd2336cd24d04d3b64d
Tiago Seabra <tiagolseabra@gmail.com>
2017-03-15 22:13:21 +0000

    another approach to i306 in Python

--- Python/intermediate/i306.py
+++ Python/intermediate/i306.py
@@ -1,10 +1,8 @@
 # 15/03/2017
-from itertools import starmap
 
 def gray_code_seq(n):
     if n == 1:
-        return [0, 1]
+        return ['0', '1']
     else:
-        f = lambda x, p: '{:{pre}>{width}}'.format(x, pre=p, width=n)
-        to_list = lambda x, p: list(starmap(f, zip(x, [p]*2**(n-1))))
-        return to_list(gray_code_seq(n-1), 0) + to_list(gray_code_seq(n-1)[::-1], 1)
+        return ['0' + i for i in gray_code_seq(n-1)] + \
+               ['1' + i for i in gray_code_seq(n-1)[::-1]]
