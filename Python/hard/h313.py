d2bb6afaf1dca02fd3af72a50c3de5b7cd0f1e2b
Tiago Seabra <tiagolseabra@gmail.com>
2017-05-14 23:08:59 +0100

    improved h313 in Python

--- Python/hard/h313.py
+++ Python/hard/h313.py
@@ -2,15 +2,18 @@
 '''
 1.  Slim the enable1 dictionary so that it only contains "not-embedded" words;
     This is done through the not_embedded_wordlist function and takes forever (~2h) to run.
-2.  Create an initial solution using a simple algorithm in the embed_word function.
-    Current initial solution has length 390.
-3.  ????
+2.  Create an initial solution using a simple algorithm in the embed_word1 function. Length ~390.
+3.  Create an initial solution using an even simpler algorithm (create_solution2). Length ~275.
+4.  ????
 '''
 
 import os.path
 import time
+from string import ascii_lowercase
 
 def get_substrings(string, n):
+    '''Returns all of string's substrings of length n'''
+
     result = []
     for i in range(1+len(string)-n):
         result.append(string[i:i+n])
@@ -55,15 +58,13 @@ def not_embedded_wordlist(all_words):
     print("This took {} seconds.".format(elapsed))
     return not_embedded
 
-def embed_word(word, solution):
+def embed_word1(word, solution):
     '''Returns a new solution so that word is now embedded.
        Explanation:
-       LATER
     '''
-
     missing_f, missing_b = [], []
 
-    for w, s, m in zip([word, word[::-1]],[solution, solution[::-1]], [missing_f, missing_b]):
+    for w, s, m in zip([word, word[::-1]], [solution, solution[::-1]], [missing_f, missing_b]):
         curr_index = 0
         for char in w:
             result = s.find(char, curr_index)
@@ -72,26 +73,46 @@ def embed_word(word, solution):
             else:
                 curr_index = result + 1
 
-    condition = len(missing_f) < len(missing_b)
-    missing = missing_f if condition else missing_b
-    new_solution = list(solution) if condition else list(solution[::-1])
+    missing = missing_f if len(missing_f) < len(missing_b) else missing_b
+    new_solution = list(solution) if len(missing_f) < len(missing_b) else list(solution[::-1])
 
     already_included = 0
     for chars, pos in missing:
         new_solution.insert(pos+already_included, chars)
         already_included += 1
 
-    return ''.join(new_solution) if condition else ''.join(new_solution[::-1])
+    return ''.join(new_solution) if len(missing_f) < len(missing_b) else ''.join(new_solution[::-1])
 
-def create_solution(wordlist):
+def create_solution1(wordlist):
     '''Creates a solution from the given wordlist.'''
 
     solution = ""
     for word in wordlist:
         if not is_embedded(word, solution):
-            solution = embed_word(word, solution)
+            solution = embed_word1(word, solution)
 
     return solution
+
+def create_solution2(wordlist):
+    '''Creates a solution from the given wordlist.'''
+
+    solution = []
+
+    while not test_valid(''.join(solution), wordlist):  # makes for a very costly loop
+        alpha_count = {l : 0 for l in ascii_lowercase}
+        for word in wordlist:
+            alpha_count[word[0]] += 1
+        top_letter = max(alpha_count, key=alpha_count.get)
+        solution.append(top_letter)
+
+        for i, word in enumerate(wordlist):
+            if word[0] == top_letter:
+                if len(word) == 1:
+                    wordlist.remove(word)
+                else:
+                    wordlist[i] = word[1:]
+
+    return ''.join(solution)
 
 def test_valid(solution, wordlist):
     '''Tests wheter or not every word in the wordlist is embedded in the solution.'''
@@ -112,6 +133,12 @@ if __name__ == "__main__":
         with open("not_embedded.txt", "r") as f:
             NEW_WORDLIST = [w.rstrip() for w in f.readlines()]
 
-    INITIAL_SOLUTION = create_solution(NEW_WORDLIST)
-    print("Initial solution's length: {}.".format(len(INITIAL_SOLUTION)))
-    print("Passes tests: {}.".format(test_valid(INITIAL_SOLUTION, NEW_WORDLIST)))
+    print("\n<<< First attempt >>>")
+    INITIAL_SOLUTION = create_solution1(NEW_WORDLIST)
+    print("Initial solution's length: {}".format(len(INITIAL_SOLUTION)))
+    print("Passes tests: {}".format(test_valid(INITIAL_SOLUTION, NEW_WORDLIST)))
+
+    print("\n<<< Second attempt >>>")
+    INITIAL_SOLUTION2 = create_solution2(NEW_WORDLIST)
+    print("Initial solution's length: {}".format(len(INITIAL_SOLUTION2)))
+    print("Passes tests: {}".format(test_valid(INITIAL_SOLUTION2, NEW_WORDLIST)))
