b1e93b4ca5507a16dd0eeec6eb244a21dfe401ec
Tiago Seabra <tiagolseabra@gmail.com>
2017-05-24 13:45:46 +0100

    e316.py code improvement

--- Python/easy/e316.py
+++ Python/easy/e316.py
@@ -1,19 +1,32 @@
 # 23/05/2017
-# Uses a very un-optimized breadth-first search algorithm
+from queue import Queue
 
-from collections import deque
-from math import log, ceil
+class Node(object):
+    def __init__(self, value, parent):
+        self.value = value
+        self.parent = parent
 
 def knight_move(x, y):
     possible_moves = [(-1, -2), (1, -2), (-1, 2), (1, 2), 
                       (-2, -1), (2, -1), (-2, 1), (2, 1)]
 
-    nodes = deque([(0, 0)])
-    counts = 0
+    nodes = Queue()
+    nodes.put(Node(tuple([0, 0]), None))
+    visited = {(0,0)}
+
     while nodes:
-        curr = nodes.popleft()
-        if curr == (x, y):
-            return ceil(log(counts, 8))
-        for node in [tuple(map(sum, zip(curr, move))) for move in possible_moves]:
-            nodes.append(node)
-        counts += 1
+        curr = nodes.get()
+        if curr.value == (x, y):
+            break
+        for node in [tuple(map(sum, zip(curr.value, move))) for move in possible_moves]:
+            if node not in visited:
+                visited |= set(node)
+                nodes.put(Node(node, parent=curr))
+                
+    path = []
+    while curr is not None:
+        path.append(str(curr.value))
+        curr = curr.parent
+    print('{}'.format(" -> ".join(path[::-1])))
+
+    return len(path) - 1
