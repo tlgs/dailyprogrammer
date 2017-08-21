4c13b701a499fc546ce7eee8bbcbb1074dd7b84f
Tiago Seabra <tiagolseabra@gmail.com>
2017-08-21 18:25:31 +0100

    updated h326 in C

--- C/hard/h326.c
+++ C/hard/h326.c
@@ -5,57 +5,6 @@
 #include <stdlib.h>
 
 #define MAX_WLENGTH 32
-#define MAX_WCOUNT 1024*1024
-
-int cmpChar(const void * a, const void * b);
-int find(char blocks[], int N,  char c, int start);
-int contains(char blocks[], int N, char word[]);
-int validate(char blocks[], int N, char words[][MAX_WLENGTH], int size);
-
-int main(void){
-    static char words[MAX_WCOUNT][MAX_WLENGTH];
-    unsigned int nwords = 0;
-    char line[MAX_WLENGTH];
-    while (fgets(line, sizeof(line), stdin)){
-        line[strlen(line)-1] = '\0';
-        strcpy(words[nwords++], line);
-    }
-
-    /* Starting from a known solution */
-    char start[] = "ehiaoslnrtaeiotcdnorsacegioucdgilmpsehilmnosubdglmnpstvbefghinprsyafghklpuvwxzabcdgjkqrtwxyadefjklnpqruyz";
-    int N = 14;
-
-    char best[N*(N+1)/2];
-    strcpy(best, start);
-    unsigned char improve;
-    do{
-        improve = 0;
-        for (int i = 0; i < N*(N+1)/2; i++){
-            char attempt[N*(N+1)/2];
-            strcpy(attempt, best);
-
-            for (int j = 97; j < best[i]; j++){
-                attempt[i] = j;
-                if (validate(attempt, N, words, nwords)){
-                    strcpy(best, attempt);
-                    improve = 1;
-                    break;
-                }
-            }
-        }
-    }
-    while (improve);
-
-    for (int i = 0; i < N; i++)
-        qsort(best + i*(i+1)/2, i+1, sizeof(char), cmpChar);
-
-    puts(best);
-}
-
-
-int cmpChar(const void* a, const void* b){
-    return *(char*)a - *(char*)b;
-}
 
 int find(char blocks[], int N,  char c, int start){
     for (int i = start; i < N; i++){
@@ -109,4 +58,8 @@ int validate(char blocks[], int N, char words[][MAX_WLENGTH], int size){
         }  
     }
     return flag;
+}
+
+int main(void){
+   return 0;
 }
