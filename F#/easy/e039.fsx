7432d9208c080e20fc4e447afc84697f1a785710
Tiago Seabra <tlgs@users.noreply.github.com>
2018-09-23 17:36:00 +0200

    added e039 in F#

---
+++ F#/easy/e039.fsx
@@ -0,0 +1,14 @@
+// 23/09/2018
+
+let fizzBuzz n =
+    [1..n]
+    |> List.map (function
+        | x when (x % 3 = 0) && (x % 5 = 0) -> "FizzBuzz"
+        | x when x % 3 = 0 -> "Fizz"
+        | x when x % 5 = 0 -> "Buzz"
+        | x -> string x)
+
+fsi.CommandLineArgs.[1]
+|> int
+|> fizzBuzz
+|> List.iter (printfn "%s")
