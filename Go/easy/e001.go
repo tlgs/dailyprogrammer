5073c649ee6a52399a0381d61b08ebb412e06765
Tiago Seabra <tlgs@users.noreply.github.com>
2019-04-11 12:07:54 +0100

    added e001 in Go

---
+++ Go/easy/e001.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name, age, username string
+	fmt.Print("name: ")
+	fmt.Scanln(&name)
+	fmt.Print("age: ")
+	fmt.Scanln(&age)
+	fmt.Print("reddit username: ")
+	fmt.Scanln(&username)
+
+	fmt.Printf("your name is %s, you are %s years old and your reddit username is /u/%s",
+		name, age, username)
+}
