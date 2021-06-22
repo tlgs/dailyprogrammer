a9f08a7180e071b7dc23d2072692355abd57b8ec
Tiago Seabra <tlgs@users.noreply.github.com>
2021-06-22 15:52:32 +0100

    feat: add e395 in Python

---
+++ Python/easy/e395.py
@@ -0,0 +1,13 @@
+# 22/06/2021
+import itertools
+
+def nonogramrow(seq):
+    return [sum(g) for k, g in itertools.groupby(seq) if k]
+
+assert nonogramrow([]) == []
+assert nonogramrow([0,0,0,0,0]) == []
+assert nonogramrow([1,1,1,1,1]) == [5]
+assert nonogramrow([0,1,1,1,1,1,0,1,1,1,1]) == [5,4]
+assert nonogramrow([1,1,0,1,0,0,1,1,1,0,0]) == [2,1,3]
+assert nonogramrow([0,0,0,0,1,1,0,0,1,0,1,1,1]) == [2,1,3]
+assert nonogramrow([1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]) == [1,1,1,1,1,1,1,1]
