b6f11944ff9c23bb2838bb3384279f67f51499bf
Tiago Seabra <tlgs@users.noreply.github.com>
2021-07-06 02:36:17 +0100

    feat: add e397 in Python

---
+++ Python/easy/e397.py
@@ -0,0 +1,14 @@
+# 06/07/2021
+
+
+def numcompare(x, y):
+    m = dict(zip("MDCLXVI", "🟥🟧🟨🟩🟦🟪🟫"))
+    return x.translate(m) < y.translate(m)
+
+
+assert numcompare("I", "I") == False
+assert numcompare("I", "II") == True
+assert numcompare("II", "I") == False
+assert numcompare("V", "IIII") == False
+assert numcompare("MDCLXV", "MDCLXVI") == True
+assert numcompare("MM", "MDCCCCLXXXXVIIII") == False
