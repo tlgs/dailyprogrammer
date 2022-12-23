e2faf9f8f9aa48a6480b2d5297d4037fe3faf1ea
Tiago Seabra <tlgs@users.noreply.github.com>
2022-12-23 17:04:58 +0000

    Add hard #1 in Python

---
+++ hard/001/main.py
@@ -0,0 +1,19 @@
+"""Hard #1"""
+
+
+def guess(low, high):
+    v = (high + low) // 2
+    print(f"Is your number {v}? (y)es, (l)ower, or (h)igher")
+    match input():
+        case "y":
+            return
+        case "l":
+            guess(low, v - 1)
+        case "h":
+            guess(v + 1, high)
+        case x:
+            raise ValueError(x)
+
+
+if __name__ == "__main__":
+    guess(1, 100)
